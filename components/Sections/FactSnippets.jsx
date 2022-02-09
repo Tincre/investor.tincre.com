6f87481e77fc941bedfb1ec5011edabce3257c2a
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-09 17:58:26 -0600

    :recycle: add href passing between sections [sc-788]

--- components/Sections/FactSnippets.jsx
+++ components/Sections/FactSnippets.jsx
@@ -31,18 +31,23 @@ export default function FactSnippets({
                   className="w-full md:w-1/2 lg:w-1/3 p-4"
                   key={`${index}-fact-snippet-card`}
                 >
-                  <div className="bg-gray-50 p-6 rounded-lg">
-                    <p className="mb-8 text-base leading-relaxed lg:text-xl lg:leading-relaxed text-gray-500">
-                      {description}
-                    </p>
-                    <img
-                      className="mb-2 h-12 w-12 object-cover rounded-full mx-auto"
-                      src={imageSrc}
-                      alt={`${title} fact snippet heading object`}
-                    />
-                    <p className="text-2xl font-bold font-heading">{title}</p>
-                    <p className="text-lg text-gray-400">{subTitle}</p>
-                  </div>
+                  <a href={`#${index}-fact-card`}>
+                    <div
+                      id={`${index}-fact-snippet-card`}
+                      className="bg-gray-50 p-6 rounded-lg"
+                    >
+                      <p className="mb-8 text-base leading-relaxed lg:text-xl lg:leading-relaxed text-gray-500">
+                        {description}
+                      </p>
+                      <img
+                        className="mb-2 h-12 w-12 object-cover rounded-full mx-auto"
+                        src={imageSrc}
+                        alt={`${title} fact snippet heading object`}
+                      />
+                      <p className="text-2xl font-bold font-heading">{title}</p>
+                      <p className="text-lg text-gray-400">{subTitle}</p>
+                    </div>
+                  </a>
                 </div>
               );
             }
