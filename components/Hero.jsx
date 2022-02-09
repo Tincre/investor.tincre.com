34042994f84b33a629b402f2b00cb0dfe5f7d694
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-09 08:33:57 -0600

    :recycle: extract cta + ctaHref props

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,8 +3,8 @@ import { signIn, signOut } from "next-auth/react";
 export default function Hero({
   heading,
   subHeading,
-  cta1,
-  cta2,
+  cta,
+  ctaHref,
   imageSrc,
   session,
 }) {
@@ -20,9 +20,9 @@ export default function Hero({
         <div>
           <a
             className="w-full lg:w-2/5 block lg:inline-block px-5 py-3 lg:mr-3 mb-3 lg:mb-0 text-sm text-center bg-indigo-500 hover:bg-indigo-600 text-white font-semibold border border-indigo-500 hover:border-indigo-600 rounded transition duration-200"
-            href="#"
+            href={ctaHref}
           >
-            {cta1}
+            {cta}
           </a>
           {!session ? (
             <button
