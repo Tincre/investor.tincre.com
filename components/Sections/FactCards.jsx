334917eb8155e06302b34fe951c88236d01f8e47
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-10 07:16:03 -0600

    :recycle: fix height for medium screens and up only

--- components/Sections/FactCards.jsx
+++ components/Sections/FactCards.jsx
@@ -11,11 +11,11 @@ export default function FactCards({ factCardsContent }) {
                 id={`${index}-fact-card`}
               >
                 <a href={`#${index}-fact-snippet-card`}>
-                  <div className="py-12 px-10 rounded-lg bg-white group-hover:bg-indigo-800 transition hover:ease-in ease-out duration-300">
+                  <div className="py-12 px-10 md:h-96 rounded-lg bg-white group-hover:bg-indigo-800 transition hover:ease-in ease-out duration-300">
                     <span className="mb-6 flex items-center justify-center w-12 h-12 rounded-full bg-indigo-500 group-hover:bg-indigo-200 text-indigo-50 group-hover:text-indigo-800 font-bold font-heading transition hover:ease-in ease-out duration-700">
                       {index + 1}
                     </span>
-                    <h3 className="mb-4 h-36 text-3xl leading-tight md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight font-bold font-heading group-hover:text-white transition hover:ease-in ease-out duration-300">
+                    <h3 className="mb-4 md:h-36 text-3xl leading-tight md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight font-bold font-heading group-hover:text-white transition hover:ease-in ease-out duration-300">
                       {title}
                     </h3>
                     <p className="text-lg text-gray-500 leading-loose group-hover:text-indigo-100 transition hover:ease-in ease-out duration-700">
