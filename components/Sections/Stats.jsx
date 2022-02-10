177d13c58c9f05f03ddb364aa5f688dcb1803207
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-10 13:24:39 -0600

    :bug: fix 'stats-1' label to 'stats'

--- components/Sections/Stats.jsx
+++ components/Sections/Stats.jsx
@@ -2,11 +2,11 @@ import StatNumberCard from "../StatNumberCard";
 
 export default function Stats({ data }) {
   return (
-    <section id="stats-1" className="py-20">
+    <section id="stats" className="py-20">
       <div className="container px-4 mx-auto">
         <div className="flex flex-wrap items-start -mx-4 -mb-8">
           {data?.map((props, index) => {
-            return <StatNumberCard {...props} key={`${index}` + "stats1"} />;
+            return <StatNumberCard {...props} key={`${index}` + "stats"} />;
           })}
         </div>
       </div>
