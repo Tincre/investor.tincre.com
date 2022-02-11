af0752c009922cb1f45a333cff0d6df7527e0c55
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-10 22:01:32 -0600

    :wrench: add additional timings for transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content : [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme : {
-    extend : {},
+  theme: {
+    extend: {
+      transitionDuration: {
+        1500: "1500ms",
+        2000: "2000ms",
+        3000: "3000ms",
+      },
+    },
   },
-  plugins : [ require('@tailwindcss/typography') ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
