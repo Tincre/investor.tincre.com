17e28b86aab54986ddeec6f0093f468b4ec0e117
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-04-01 14:10:43 -0500

    :sparkles: add a simple learn more component to replace silu process

---
+++ components/Sections/LearnMore.jsx
@@ -0,0 +1,12 @@
+export default function SafeTerms({ buttonText }) {
+  return (
+    <div className="mx-4 md:w-2/5 md:mx-auto md:pb-12 lg:w-1/2 lg:mx-auto lg:pb-24">
+      <a
+        className="w-full text-center md:px-12 block px-5 py-3 text-sm font-semibold text-indigo-500 hover:text-white hover:bg-indigo-500 border border-indigo-500 hover:border-indigo-600 rounded transition duration-200"
+        href="https://tincre.com"
+      >
+        Learn more about Tincre Promo
+      </a>
+    </div>
+  );
+}
